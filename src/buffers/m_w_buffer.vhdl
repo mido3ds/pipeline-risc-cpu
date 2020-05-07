22988d5dff58fd75c50a8bc9c6aae8213f8f60d5
Evraa <evramyousef@gmail.com>
2020-05-07 18:43:57 +0200

    BUFFER updated buffer m_w_buffer to handle the swap case and added data section

--- src/buffers/m_w_buffer.vhdl
+++ src/buffers/m_w_buffer.vhdl
@@ -7,14 +7,18 @@ entity m_w_buffer is
         clk             : in std_logic;
         in_aluout       : in std_logic_vector(32 - 1 downto 0);
         in_mem          : in std_logic_vector(32 - 1 downto 0);
+        in_data         : in std_logic_vector(32 - 1 downto 0);
         in_opcode       : in std_logic_vector(7 - 1 downto 0);
-        in_destination  : in std_logic_vector(4 - 1 downto 0);
+        in_destination_0: in std_logic_vector(4 - 1 downto 0);
+        in_destination_1: in std_logic_vector(4 - 1 downto 0);
         in_interrupt    : in std_logic;
 
         out_aluout      : out std_logic_vector(32 - 1 downto 0);
         out_mem         : out std_logic_vector(32 - 1 downto 0);
+        out_data        : out std_logic_vector(32 - 1 downto 0);
         out_opcode      : out std_logic_vector(7 - 1 downto 0);
-        out_destination : out std_logic_vector(4 - 1 downto 0);
+        out_destination_0 : out std_logic_vector(4 - 1 downto 0);
+        out_destination_1 : out std_logic_vector(4 - 1 downto 0);
         out_interrupt   : out std_logic
     );
 end entity;
@@ -22,14 +26,18 @@ end entity;
 architecture rtl of m_w_buffer is
     signal aluout      : std_logic_vector(32 - 1 downto 0);
     signal mem         : std_logic_vector(32 - 1 downto 0);
+    signal data         : std_logic_vector(32 - 1 downto 0);
     signal opcode      : std_logic_vector(7 - 1 downto 0);
-    signal destination : std_logic_vector(4 - 1 downto 0);
+    signal destination_0 : std_logic_vector(4 - 1 downto 0);
+    signal destination_1 : std_logic_vector(4 - 1 downto 0);
     signal interrupt   : std_logic;
 begin
     aluout      <= in_aluout;
     mem         <= in_mem;
+    data        <= in_data;
     opcode      <= in_opcode;
-    destination <= in_destination;
+    destination_0 <= in_destination_0;
+    destination_1 <= in_destination_1;
     interrupt   <= in_interrupt;
 
     process (clk)
@@ -37,8 +45,10 @@ begin
         if rising_edge(clk) then
             out_aluout      <= aluout;
             out_mem         <= mem;
+            out_data        <= data;
             out_opcode      <= opcode;
-            out_destination <= destination;
+            out_destination_0 <= destination_0;
+            out_destination_1 <= destination_1;
             out_interrupt   <= interrupt;
         end if;
     end process;
