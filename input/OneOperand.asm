419fe039113ecf4d7ca8fcb980c5bfffbd7a50da
Mahmoud Adas <mido3ds@gmail.com>
2020-05-20 04:49:22 +0200

    added given test cases to input

--- input/OneOperand.asm
+++ input/OneOperand.asm
@@ -9,16 +9,13 @@
 100
 
 .ORG 10
-PUSH R4        #push R4 into stack
-NOP            #No change
-SUB R4, R6, R2 #subtract R2 from R6 and store in R4
 NOT R1         #R1 =FFFFFFFF , C--> no change, N --> 1, Z --> 0
-inc R1         #R1 =00000000 , C --> 1 , N --> 0 , Z --> 1
-in R1          #R1= 5,add 5 on the in port,flags no change	
+NOP            #No change
+inc R1	       #R1 =00000000 , C --> 1 , N --> 0 , Z --> 1
+in R1	       #R1= 5,add 5 on the in port,flags no change	
 in R2          #R2= 10,add 10 on the in port, flags no change
-NOT R2         #R2= FFFFFFEF, C--> no change, N -->1,Z-->0
+NOT R2	       #R2= FFFFFFEF, C--> no change, N -->1,Z-->0
 inc R1         #R1= 6, C --> 0, N -->0, Z-->0
-Dec R2         #R2= FFEE,C-->1 , N-->1, Z-->0
+Dec R2         #R2= FFFFFFEE,C-->1 , N-->1, Z-->0
 out R1
 out R2
-end
